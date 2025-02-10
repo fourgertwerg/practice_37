0d0d5e9b5c157cf1805c1ed77cfcc5475f137d49
4gertwerg <131755084+fourgertwerg@users.noreply.github.com>
2025-02-10 17:08:12 +0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+   module.exports = {
+     "env": {
+       "browser": true,
+       "es2021": true
+     },
+     "extends": [
+       "eslint:recommended",
+       "plugin:react/recommended"
+     ],
+     "overrides": [
+     ],
+     "parserOptions": {
+       "ecmaVersion": "latest",
+       "sourceType": "module"
+     },
+     "plugins": [
+       "react"
+     ],
+     "rules": {
+     }
+   };
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+   
